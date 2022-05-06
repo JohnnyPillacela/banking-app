75d93e0dc3d1212cfc0e1685f358c0de6540a6e5
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-06 18:48:55 -0400

    Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavDropdown, Nav, Button, Container, Navbar } from 'react-bootstrap';
+// import { BrowserRouter, Route} from "react-router-dom";
+
+
+export default function NavBar() {
+  return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Johnny's Bank</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar" />
+                <Navbar.Collapse id="navbar">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link"></Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+  );
+}
