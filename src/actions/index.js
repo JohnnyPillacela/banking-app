4de451c15341aaeac503ab4dc55dac4309c184aa
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-07 00:57:20 -0400

    actions index

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+export const wdithdraw = (_id, accountId, type, amount, name) => {
+  return {
+    type: "withdraw",
+    payload: {
+      _id,
+      accountId,
+      type,
+      amount,
+      name,
+    },
+  };
+};
+
+export const deposit = (_id, accountId, type, amount, name) => {
+    return {
+      type: "deposit",
+      payload: {
+        _id,
+        accountId,
+        type,
+        amount,
+        name,
+      },
+    };
+  };
+
+export const retrieve_accounts = (accounts, transactions) => {
+  return {
+    type: "retrieve_accounts",
+    payload: {
+        accounts,
+        transactions
+    }
+  };
+};
