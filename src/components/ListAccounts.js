b23e83f6ee10c09ab95cca7646b10ea5904fdc5b
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-07 09:11:15 -0400

    get accounts and transactions and save to redux

--- src/components/ListAccounts.js
+++ src/components/ListAccounts.js
@@ -1,22 +1,59 @@
 import React from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 
-export default class ListAccounts extends React.Component {
-
-  getData() {
-    axios
-      .get("https://my-json-server.typicode.com/bnissen24/project2DB/accounts")
-      .then((response) => {
-        this.setState({
-          tasks: response.data,
-          sorted_tasks: this.sortTasks(response.data),
+const accountsAPI =
+  "https://my-json-server.typicode.com/bnissen24/project2DB/accounts";
+const transactionsAPI =
+  "https://my-json-server.typicode.com/bnissen24/project2DB/transactions";
+class ListAccounts extends React.Component {
+  //  usses promises
+  getAccounts = () => {
+    return (dispatch) => {
+      return axios
+        .get(accountsAPI)
+        .then((response) => {
+          return response.data;
+        })
+        .then((data) => {
+          dispatch({
+            type: "retrieve_accounts",
+            payload: data,
+          });
+        })
+        .catch((error) => {
+          throw error;
         });
-      })
-      .catch((error) => {
-        this.setState({ errorMessage: error.message });
-      });
-  }
+    };
+  };
+
+  //  uses async await
+  getTransactions = () => {
+    return async (dispatch) => {
+      try {
+        const response = await axios.get(transactionsAPI);
+        const data = response.data;
+        dispatch({
+          type: "retrieve_transactions",
+          payload: data,
+        });
+      } catch (error) {
+        throw error;
+      }
+    };
+  };
+
   render() {
-    return <div></div>;
+    return;
+    <div></div>;
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    accounts: state.accounts,
+    transactions: state.transactions,
+  };
+};
+
+export default connect(mapStateToProps)(ListAccounts);
