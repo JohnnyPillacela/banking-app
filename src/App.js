983ff906a0deb2fb5595ad0f742f3ddf57636059
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-08 17:22:18 -0400

    changed to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { Container } from "react-bootstrap";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { add_accounts, add_transactions } from "./actions";
 import { connect } from "react-redux";
 
@@ -16,50 +16,53 @@ const accountsAPI =
 const transactionsAPI =
   "https://my-json-server.typicode.com/bnissen24/project2DB/transactions";
 
-function App(props) {
+class App extends React.Component {
+  constructor(props) {
+    console.log("Inside APP");
+    super(props);
+    this.getAccounts();
+    this.getTransactions();
+  }
 
-  const getAccounts = async () => {
+  async getAccounts() {
     try {
       console.log("Inside of getAccounts");
       const response = await axios.get(accountsAPI);
-      props.add_accounts(response.data);
+      this.props.add_accounts(response.data);
     } catch (error) {
       throw error;
     }
   }
 
-  const getTransactions = async () => {
+  async getTransactions() {
     try {
       console.log("Inside of getTransactions");
       const response = await axios.get(transactionsAPI);
-      props.add_transactions(response.data);
+      this.props.add_transactions(response.data);
     } catch (error) {
       throw error;
     }
   }
 
-  useEffect(() => {
-    getAccounts();
-    getTransactions();
-  })
-
-  return (
-    <Container className="app">
-      <BrowserRouter>
-        <NavBar />
-        <Container>
-          <h1>Welcome, Bank Manager</h1>
-          {/* <ListAccounts /> */}
-        </Container>
-        <Routes>
-          <Route path="/" exact element={<ListAccounts />} />
-          <Route path="/accounts" element={<ListAccounts />} />
-          <Route path="/transactions" element={<ListTransactions />} />
-          <Route path="/account/:accountId" element={<UserAccount />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
-  );
+  render() {
+    return (
+      <Container className="app">
+        <BrowserRouter>
+          <NavBar />
+          <Container>
+            <h1>Welcome, Bank Manager</h1>
+            {/* <ListAccounts /> */}
+          </Container>
+          <Routes>
+            <Route path="/" exact element={<ListAccounts />} />
+            <Route path="/accounts" element={<ListAccounts />} />
+            <Route path="/transactions" element={<ListTransactions />} />
+            <Route path="/account/:accountId" element={<UserAccount />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
