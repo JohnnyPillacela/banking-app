908ef23e86cd5378f5ab2e9b298da2d43a7214f0
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-08 12:25:15 -0400

    reordered where api is being called

--- src/components/ListAccounts.js
+++ src/components/ListAccounts.js
@@ -1,48 +1,10 @@
 import React from "react";
-import axios from "axios";
 import { connect } from "react-redux";
-import { add_accounts, add_transactions } from "../actions";
 
 import ListAccountItem from "./ListAccountItem";
 import "../styles/ListAccounts.css";
-import { Container } from "react-bootstrap";
-
-const accountsAPI =
-  "https://my-json-server.typicode.com/bnissen24/project2DB/accounts";
-const transactionsAPI =
-  "https://my-json-server.typicode.com/bnissen24/project2DB/transactions";
 class ListAccounts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getAccounts();
-    this.getTransactions();
-  }
-
-  async getAccounts() {
-    try {
-      console.log("Inside of getAccounts");
-      const response = await axios.get(accountsAPI);
-      this.props.add_accounts(response.data);
-      this.setState({});
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async getTransactions() {
-    try {
-      console.log("Inside of getTransactions");
-      const response = await axios.get(transactionsAPI);
-      this.props.add_transactions(response.data);
-    } catch (error) {
-      throw error;
-    }
-  }
-
   render() {
-    // console.log("Inside Render");
-    // console.log(this.props);
-    //let accountsList = this.renderList();
     return (
       <div className="accounts">
         <h3>Accounts List</h3>
@@ -69,11 +31,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    add_accounts: (accounts) => dispatch(add_accounts(accounts)),
-    add_transactions: (transactions) =>
-      dispatch(add_transactions(transactions)),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(ListAccounts);
+export default connect(mapStateToProps, null)(ListAccounts);
