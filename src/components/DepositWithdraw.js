579958fe0f926f13ad1d6f019fba0dc2eebf16de
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-08 10:15:36 -0400

    made withdraw and deposit view only

---
+++ src/components/DepositWithdraw.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Connect } from "react-redux";
+
+export default function DepositWithdraw() {
+  return (
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1"></label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Transaction Name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1"></label>
+        <input
+          type="number"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Amount"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/components/ListAccountItem.js
+++ src/components/ListAccountItem.js
@@ -2,21 +2,31 @@ import React from "react";
 import { connect } from "react-redux";
 import { Card } from "react-bootstrap";
 
-import '../styles/ListAccountItem.css';
+import DepositWithdraw from "./DepositWithdraw";
+import "../styles/ListAccountItem.css";
 
 const ListAccountItem = (props) => {
-//   console.log("Each Item");
-//   console.log(props.account);
+  //   console.log("Each Item");
+  //   console.log(props.account);
   const account = props.account;
   return (
-    <Card className="account" key={account._id}>
-      <Card.Body>
-        <Card.Title className="card-title">{account.name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          <b>Balance:</b> ${account.balance}
-        </Card.Subtitle>
-      </Card.Body>
-    </Card>
+    <div className="row account">
+      <Card className="col-5 border-0" key={account._id}>
+        <Card.Body className="card-body">
+          <Card.Title className="card-title">{account.name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            <b>Balance:</b> ${account.balance}
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+      <Card className="col-7 border-0">
+        <Card.Body className="card-body">
+          <Card.Text className="card-text">
+            <DepositWithdraw/>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
