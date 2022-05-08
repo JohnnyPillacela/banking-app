023f6167246a2e564d2753dcbff27269d34fe747
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-08 11:24:44 -0400

    retrieved transactions

--- src/components/ListTransactions.js
+++ src/components/ListTransactions.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import ListTransactionsItem from "./ListTransactionsItem";
 import "../styles/ListTransactions.css";
 
 function ListTransactions(props) {
+  console.log("Inside Transaction Component");
   console.log(props);
   const transactions = props.transactions;
   console.log(transactions);
   return (
     <div className="transactions">
       <h3>Your Customers Transactions</h3>
+      <hr />
+      <div className="accounts-list">
+        {props.transactions.map((transaction) => {
+          return (
+            <ListTransactionsItem
+              transaction={transaction}
+              key={transaction._id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/ListTransactionsItem.js
+++ src/components/ListTransactionsItem.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-import DepositWithdraw from "./DepositWithdraw";
-import "../styles/ListAccountItem.css";
+import "../styles/ListTransactionsItem.css";
 
-const ListAccountItem = (props) => {
+const ListTransactionsItem = (props) => {
   //   console.log("Each Item");
-  //   console.log(props.account);
-  const account = props.account;
+  //   console.log(props.transaction);
+  const transaction = props.transaction;
   return (
-    <div className="row account">
-      <Card className="col-5 border-0" key={account._id}>
+    <div className="row transaction">
+      <Card className="col-5 border-0" key={transaction._id}>
         <Card.Body className="card-body">
-          <Card.Title className="card-title">{account.name}</Card.Title>
+          <Card.Title className="card-title">{transaction.name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            <b>Balance:</b> ${account.balance}
+            <b>Amount:</b> ${transaction.amount}
+            <b>Type:</b> ${transaction.type}
           </Card.Subtitle>
         </Card.Body>
       </Card>
       <Card className="col-7 border-0">
         <Card.Body className="card-body">
           <Card.Text className="card-text">
-            <DepositWithdraw/>
           </Card.Text>
         </Card.Body>
       </Card>
@@ -29,4 +28,4 @@ const ListAccountItem = (props) => {
   );
 };
 
-export default ListAccountItem;
+export default ListTransactionsItem;
