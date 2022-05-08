5231713f9d20e78db9142bba233ddfa43f8da2b1
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-08 11:12:35 -0400

    linked to correct component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import axios from "axios";
 import { Container } from "react-bootstrap";
-import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import ListAccounts from "./components/ListAccounts";
+import ListTransactions from "./components/ListTransactions";
 import "./styles/App.css";
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<ListAccounts />} />
           <Route path="/accounts" element={<ListAccounts />} />
-          <Route path="/transactions" element={<ListAccounts />} />
+          <Route path="/transactions" element={<ListTransactions />} />
         </Routes>
       </BrowserRouter>
     </Container>
