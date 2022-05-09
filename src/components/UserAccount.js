f22b6d61e605a3ac1a9bb3d70b32816a3a6492bd
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-09 01:32:34 -0400

    removed unused imports

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 
 import ListAccountItem from "./ListAccountItem";
 import ListTransactionsItem from "./ListTransactionsItem";
